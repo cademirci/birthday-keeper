c0b5f42f6f94c1b1eed3218023ad2d405c98a3d5
Çağlayan <demircicaglayan13@gmail.com>
2022-02-13 15:59:09 +0300

    implement get and post functions for root

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,54 @@
 const express = require('express')
 const router = express.Router()
+const Person = require('../models/person')
+const sequelize = require('sequelize')
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    name: 'junior developer assignment'
+router.get('/', async (req, res, next) => {
+  let people = [], birthdayToday = [], birthdayThisMonth = []
+
+  // find everyone
+  await Person.findAll({})
+    .then(result => people = result)
+    .catch(next)
+  
+  // find birthdates in current month
+  let currentMonth = new Date().getMonth() + 1
+  if (currentMonth < 10) {
+    currentMonth = '-0' + currentMonth + '-'
+  }
+  else {
+    currentMonth = '-' + currentMonth + '-'
+  }
+  await Person.findAll({
+    where: { birthdate: { [sequelize.Op.substring]: currentMonth }}
   })
+    .then(result => birthdayThisMonth = result)
+    .catch(err => console.log(err))
+
+  // find birthdays today
+  let currentMonthAndDay = currentMonth + new Date().getDate()
+  await Person.findAll({
+    where: { birthdate: { [sequelize.Op.substring]: currentMonthAndDay }}
+  })
+    .then(result => birthdayToday = result)
+    .catch(err => console.log(err))
+
+  res.render('index', {
+    name: 'junior developer assignment', 
+    peopleArray: people, 
+    thisMonthArray: birthdayThisMonth,
+    birthdayArray: birthdayToday
+  })
+})
+
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  return await Person.create({
+    name: req.body.name, 
+    birthdate: req.body.birthdate
+  }).then(() => {
+    res.redirect('/')
+  }).catch(err => console.log(err))
 })
 
 module.exports = router
